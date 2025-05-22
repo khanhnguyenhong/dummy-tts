23a3c1aa70c040beeca4c9e71caa58ab5f33ae72
Khanh Nguyen <khanhnguyen@Khanh-Nguyen-MacBook-Pro.local>
2025-05-22 14:06:31 +0700

    add features

--- src/components/TTVFetcher.js
+++ src/components/TTVFetcher.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { isEmpty } from "lodash";
 
 const TTVFetcher = () => {
+    // https://tangthuvien.net/get-4-chap?story_id=38060&sort_by_ttv=10
     const [fetchedData, setFetchedData] = React.useState("");
+    const [currentLink, setCurrentLink] = React.useState("");
+    const [storyId, setStoryId] = React.useState("");
+    const [chapter, setChapter] = React.useState(1);
 
-    const fetchData = () => {
-        let url = document.getElementById("input-url").value;
+    const fetchData = (input = '') => {
+        const url = isEmpty(input) ? currentLink : input;
+        localStorage.setItem('preTTVLink', url);
+        setFetchedData("Fetching " + url);
         fetch("/api/fetch-data", {
             method: "POST",
             headers: {
@@ -21,6 +28,12 @@ const TTVFetcher = () => {
         });
     }
 
+    const loadLink = () => {
+        const newLink = localStorage.getItem('preTTVLink');
+        setCurrentLink(newLink);
+        setCustomLinkData(newLink);
+    }
+
     const retriveData = () => {
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
@@ -33,16 +46,70 @@ const TTVFetcher = () => {
         xhttp.send();
     }
 
+    const setCustomLinkData = (newLink) => {
+        const qs = require('qs');
+        const params = qs.parse(newLink.split('?')[1]);
+        console.log(params.story_id);
+        console.log(params.sort_by_ttv);
+        setStoryId(params.story_id);
+        setChapter(params.sort_by_ttv);
+    }
+
+    const onChangeCurrentLink = (e) => {
+        const newLink = e.target.value;
+        setCurrentLink(newLink);
+        setCustomLinkData(newLink);
+    }
+
+    const editLink = ({ newStoryId, newChapter }) => {
+        const newLink = `https://tangthuvien.net/get-4-chap?story_id=${newStoryId || storyId}&sort_by_ttv=${newChapter || chapter}`
+        setCurrentLink(newLink);
+    }
+
+    const onChangeStoryId = (e) => {
+        setStoryId(e.target.value);
+        editLink({
+            newStoryId: e.target.value
+        })
+    }
+
+    const onChangeChapter = (e) => {
+        setChapter(e.target.value);
+        editLink({
+            newChapter: e.target.value
+        })
+    }
+
+    const fetchNextChapters = (number) => {
+        const nextChapter = parseInt(chapter) + number;
+        setChapter(nextChapter);
+        editLink({
+            newChapter: nextChapter
+        })
+        const newLink = `https://tangthuvien.net/get-4-chap?story_id=${storyId}&sort_by_ttv=${nextChapter}`
+        fetchData(newLink);
+    }
+
     return (
         <div style={{ display: "flex", flexDirection: "column", width: "300px", margin: "auto" }}>
-            <textarea type="text" placeholder="Url" id="input-url" />
-            <button type="button" onClick={fetchData}>
+            <button type="button" onClick={loadLink}>
+                Load Previous Link
+            </button>
+            <textarea type="text" placeholder="Url" value={currentLink} onChange={onChangeCurrentLink} />
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <input type="text" placeholder="story_id" value={storyId} onChange={onChangeStoryId} />
+                <input type="text" placeholder="chapter" value={chapter} onChange={onChangeChapter} />
+            </div>
+            <button type="button" onClick={() => fetchData()}>
                 Fetch
+            </button>
+            <button type="button" onClick={() => fetchNextChapters(4)}>
+                Fetch next 4 chapters
             </button>
             <button type="button" onClick={retriveData}>
                 Show data
             </button>
-            <div dangerouslySetInnerHTML={{ __html: fetchedData }}>
+            <div style={{ maxHeight: "400px", overflow: "auto" }} dangerouslySetInnerHTML={{ __html: fetchedData }}>
             </div>
         </div >
     )
